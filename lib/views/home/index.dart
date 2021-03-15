5b3dbe9760c5097fa19805576f2821d163e9b9df
李观保 <sma2lbao@qq.com>
2021-03-15 18:00:00 +0800

    home paeg: add state bottom navigation.

--- lib/views/home/index.dart
+++ lib/views/home/index.dart
@@ -1,33 +1,52 @@
-
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
 
   static final String route = '/';
+
+  @override
+  State<StatefulWidget> createState() => _Home();
+  
+}
+
+class _Home extends State<Home> {
+
+  int _selected = 0;
+  List<Widget> _bottomPages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomPages..add(Text('首页'))..add(Text('我的'));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selected = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // Scaffold is a layout for the major Material Components.
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
+        title: Text('Miss'),
       ),
       // body is the majority of the screen.
       body: Center(
-        child: Text('Hello, world!'),
+        child: _bottomPages[_selected],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
+        ],
+        currentIndex: _selected,
+        onTap: _onItemTapped,
+        fixedColor: Colors.blue,
+      ),
+     
     );
   }
   
